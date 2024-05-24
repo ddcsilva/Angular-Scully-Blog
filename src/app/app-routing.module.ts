e35c9842014d461cfaaa6966c0e1527788d07ecc
danilo.silva <danilo.silva@msn.com>
2024-05-24 19:54:28 -0300

    Adicionando o Scully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'contato', component: ContatoComponent },
   { path: 'artigos', loadChildren: () => import('./artigos/artigos.module').then(m => m.ArtigosModule) },
   { path: '', pathMatch: 'full', redirectTo: 'artigos' },
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
   { path: '**', redirectTo: 'artigos' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ContatoModule } from './contato/contato.module';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContatoModule } from './contato/contato.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    ContatoModule
+    ContatoModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
